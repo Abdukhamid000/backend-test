9f5468cd98f5caf973610fa269f10325a5280a12
Abduhamid <Abdukhamid000@gmail.com>
2024-02-23 09:33:01 +0500

    fix: files

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,23 +1,21 @@
-import jwt from 'jsonwebtoken'
-import dotenv from 'dotenv'
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export function authenticateToken(req, res, next) {
+  const token = req.header("Authorization")?.split(" ")[1];
+  console.log(req.header("Authorization"));
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
 
- const token = req.header('Authorization')?.split(' ')[1]
- console.log(req.header('Authorization'))
- if(!token) {
-    return res.status(401).json({error: 'Unauthorized'})
- }
- 
- jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-    if(err) {
-        return res.status(403).json({error: 'Invalid token'})
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Invalid token" });
     }
 
-    req.user = user
-    next()
- })
-  
+    req.user = user;
+    next();
+  });
 }
